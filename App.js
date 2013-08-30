32d33bad42b573a5074b1bc11c1ee7e6d7203727
Barry Mullan <mullan@gmail.com>
2013-08-30 12:01:58 -0400

    t

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 // <script type="text/javascript" src="https://rally1.rallydev.com/apps/2.0rc1/sdk-debug.js"></script>
-
 // <script type="text/javascript" src="https://rally1.rallydev.com/apps/2.0rc1/lib/analytics/analytics-all.js"></script>
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
